716478078251e232f88fa1580e77a158dd5d55ee
Rhuu4n <Rhuan.padua@hotmail.com>
2024-07-07 21:56:50 -0300

    função de mudança de cores feita, e mais alguns teste com javascript.

--- app/jogos/termo/termo.js
+++ app/jogos/termo/termo.js
@@ -70,10 +70,32 @@ linhas[5].inputs.map(function(event) {
     document.getElementById('linha').appendChild(event);});
 
 function VerificaPalavra(){
-    let palavra = inputs.map(event => event.value).join('');
-    alert(palavra);
+  for (i = 0; i < linhas.length; i++) {
+    let palavra = linhas[i].inputs[0].value + linhas[i].inputs[1].value + linhas[i].inputs[2].value + linhas[i].inputs[3].value + linhas[i].inputs[4].value;
+    
+    if (palavra == palavra_certa){
+      alert("ACERTOU!!");
+      confereCor(0, 0);
+    }else{
+      confereCor(0, 0);
+    }
+  }
+}
 
-    if (palavra == palavra_certa[0]){
-    alert("ACERTOU!!");
-    };
+function confereCor(linha, palavra) {
+  for (i = 0; i < linhas[linha].inputs.length; i++) {
+    if (linhas[linha].inputs[i].style.background != "green"){
+      if (linhas[linha].inputs[i].value == palavra_certa[palavra].charAt(0) ||
+          linhas[linha].inputs[i].value == palavra_certa[palavra].charAt(1) ||
+          linhas[linha].inputs[i].value == palavra_certa[palavra].charAt(2) ||
+          linhas[linha].inputs[i].value == palavra_certa[palavra].charAt(3) ||
+          linhas[linha].inputs[i].value == palavra_certa[palavra].charAt(4)) {
+          linhas[linha].inputs[i].style.background = "rgb(207, 204, 0)"
+      }
+
+      if (linhas[linha].inputs[i].value == palavra_certa[palavra].charAt(i)){
+        linhas[linha].inputs[i].style.background = "green"
+      }
+    }
+  }
 }
